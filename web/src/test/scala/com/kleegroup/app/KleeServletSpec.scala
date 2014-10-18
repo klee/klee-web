6ca66a7e72d8a9551ab7861e9d09112369e3d112
Ainsley Escorce-Jones <me@ains.co>
2014-10-18 21:10:55 +0100

    Added flask development to vagant environment

--- web/src/test/scala/com/kleegroup/app/KleeServletSpec.scala
+++
@@ -1,8 +0,0 @@
-package com.kleegroup.app
-
-import org.scalatra.test.specs2._
-
-// For more on Specs2, see http://etorreborre.github.com/specs2/guide/org.specs2.guide.QuickStart.html
-class KleeServletSpec extends ScalatraSpec {
-
-}
