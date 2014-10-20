ae39ea51fa6b7a623335ba3c84b426f8285890c5
ben-chin <ben.chin101@gmail.com>
2014-10-20 16:20:47 +0100

    Nothing even getting run

--- python/web/web.py
+++ python/web/web.py
@@ -1,19 +1,50 @@
 import os
 
 from flask import Flask, request, render_template
+from celery import Celery
 
 app = Flask(__name__)
 app.debug = os.environ.get("DEVELOPMENT") is not None
+app.config.update(
+    BROKER_URL=os.environ.get("RABBITMQ_HOST"),
+    RESULT_BACKEND=os.environ.get("RABBITMQ_HOST")
+)
+
+
+def make_celery(app):
+    celery = Celery(app.import_name, broker=app.config['BROKER_URL'])
+    celery.conf.update(app.config)
+    TaskBase = celery.Task
+
+    class ContextTask(TaskBase):
+        abstract = True
+
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return TaskBase.__call__(self, *args, **kwargs)
+
+    celery.Task = ContextTask
+    return celery
+
+celery = make_celery(app)
+
+
+@celery.task
+def add_together(a, b):
+    return a + b
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        # Do upload to celery
         code = request.form['code']
+        # Do upload to celery
+        task = add_together.apply_async(5, 6)
+        task_id = task.task_id
 
-        
-        return request.form['code']
+        result = add_together.AsyncResult(task_id).get(timeout=1.0)
+
+        return result
     else:
         return render_template('index.html')
 
