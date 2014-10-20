be830c8aa1e70169487fa7a3ce2b7221b090c172
ben-chin <ben.chin101@gmail.com>
2014-10-20 11:18:07 +0100

    Basic Flask skeleton

--- web/web.py
+++ web/web.py
@@ -1,14 +1,21 @@
 import os
-from flask import Flask
+
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 app.debug = os.environ.get("DEVELOPMENT") is not None
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello Testing world'
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        # Do upload to celery
+        code = request.form['code']
 
+        
+        return request.form['code']
+    else:
+        return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
