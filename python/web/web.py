25b44e482482cf420bbc4d6e83e8b24d7a2237bf
James Carr <jc3212@ic.ac.uk>
2014-10-23 10:47:08 +0100

    Now able to submit files to KLEE

--- python/web/web.py
+++ python/web/web.py
@@ -2,22 +2,29 @@ import os
 
 from flask import Flask, request, render_template
 from ..worker.worker import submit_code
-
 app = Flask(__name__)
+
 app.debug = os.environ.get("DEVELOPMENT") is not None
+
+
+def submit_task(code):
+    task = submit_code.delay(code)
+    task_id = task.task_id
+    result = submit_code.AsyncResult(task_id).get(timeout=30.0)
+    return render_template('result.html', result=result)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        code = request.form['code']
+        f = request.files['file']
+        if f:
+            code = f.read()
+            f.close()
+        else:
+            code = request.form['code']
 
-        # Submit task to queue
-        task = submit_code.delay(code)
-        task_id = task.task_id
-        result = submit_code.AsyncResult(task_id).get(timeout=30.0)
-
-        return render_template('result.html', result=result)
+        return submit_task(code)
     else:
         return render_template('index.html')
 
