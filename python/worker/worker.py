403be07a48fef3eed80e13e12bd3aae207af09cc
James Carr <jc3212@ic.ac.uk>
2014-10-23 22:24:19 +0100

    Refactored different components of KLEE run
    Extracted each of the three separate components of running KLEE into
    their own methods. Did a trial run and all seems to work.

--- python/worker/worker.py
+++ python/worker/worker.py
@@ -4,10 +4,38 @@ import subprocess
 import shutil
 from boto.s3.connection import S3Connection
 from boto.s3.key import Key
-
 from celery import Celery
 
 celery = Celery(broker=os.environ["BROKER_URL"], backend="rpc")
+
+
+def run_klee(docker_command):
+    llvm_command = ['/src/llvm-gcc4.2-2.9-x86_64-linux/bin/llvm-gcc',
+                    '-I', '/src/klee/include', '--emit-llvm', '-c', '-g', '/code/result.c',
+                    '-o', '/code/result.o']
+    klee_command = ["klee", "/code/result.o"]
+
+    subprocess.check_output(docker_command + llvm_command)
+    klee_result = subprocess.check_output(docker_command + klee_command)
+    return klee_result
+
+
+def compress_output(docker_command, file_name):
+    tar_command = ['tar', '-zcvf', file_name, '/code/klee-out-0']
+    subprocess.check_output(docker_command + tar_command)
+
+
+def upload_result(file_name, tempdir):
+    conn = S3Connection()
+    bucket = conn.get_bucket('klee-output')
+
+    k = Key(bucket)
+    k.key = file_name
+    k.set_contents_from_filename("{0}/{1}".format(tempdir, file_name))
+    k.set_acl('public-read')
+
+    url = k.generate_url(expires_in=0, query_auth=False)
+    return url
 
 
 @celery.task(name='submit_code', bind=True)
@@ -20,26 +48,12 @@ def submit_code(self, code):
             f.flush()
 
             docker_command = ['sudo', 'docker', 'run', '-t', '-v', '{}:/code'.format(tempdir), 'kleeweb/klee']
-            llvm_command = ['/src/llvm-gcc4.2-2.9-x86_64-linux/bin/llvm-gcc',
-                            '-I', '/src/klee/include', '--emit-llvm', '-c', '-g', '/code/result.c',
-                            '-o', '/code/result.o']
-            klee_command = ["klee", "/code/result.o"]
-            tar_command = ['tar', '-zcvf', '/code/klee-output-{}.tar.gz'.format(task_id), '/code/klee-out-0']
 
-            subprocess.check_output(docker_command + llvm_command)
+            file_name = 'klee-output-{}.tar.gz'.format(task_id)
 
-            klee_result = subprocess.check_output(docker_command + klee_command)
-            subprocess.check_output(docker_command + tar_command)
-
-            conn = S3Connection()
-            bucket = conn.get_bucket('klee-output')
-            k = Key(bucket)
-
-            k.key = str(task_id)
-            k.set_contents_from_filename('{0}/klee-output-{1}.tar.gz'.format(tempdir, task_id))
-            k.set_acl('public-read')
-
-            url = k.generate_url(expires_in=0, query_auth=False)
+            klee_result = run_klee(docker_command)
+            compress_output(docker_command, "/code/{}".format(file_name))
+            url = upload_result(file_name, tempdir)
 
             return klee_result.strip() + url
     except subprocess.CalledProcessError as e:
