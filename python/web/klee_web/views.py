13c54c747a540f4eb9a95af67281120d19324c3b
Ainsley Escorce-Jones <me@ains.co>
2014-10-26 21:43:26 +0000

    Migrated from flask to django

---
+++ python/web/klee_web/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+
+from forms import SubmitJobForm
+
+from worker.worker import submit_code
+
+
+def index(request):
+    if request.method == "POST":
+        form = SubmitJobForm(request.POST, request.FILES)
+        if form.is_valid():
+            code = form.cleaned_data["code"]
+            email = form.cleaned_data["email"]
+
+            uploaded_file = request.FILES.get('file')
+            if uploaded_file:
+                code = uploaded_file.read()
+
+            task = submit_code.delay(code, email)
+
+            result = submit_code.AsyncResult(task.task_id).get(timeout=30.0)
+            return render(request, "klee_web/result.html", {
+                "result": result
+            })
+    else:
+        form = SubmitJobForm()
+    return render(request, "klee_web/index.html", {"form": form})
