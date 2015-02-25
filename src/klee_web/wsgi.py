5a61ce0a4bfcf6523916268bc21d5a1dd89762b7
Ainsley Escorce-Jones <me@ains.co>
2015-02-25 14:47:22 +0000

    Set settings module before instantiating Django application

--- src/klee_web/wsgi.py
+++ src/klee_web/wsgi.py
@@ -10,6 +10,6 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+application = get_wsgi_application()
