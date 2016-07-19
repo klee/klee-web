a9cd43072bc095780f399f62aa94c45baaf81953
Domenico Fabio Marino <dfm114@ic.ac.uk>
2016-07-19 10:24:33 +0100

    Klee_Web: Update NodeJS to 6.x
    Signed-off-by: Domenico Fabio Marino <dfm114@ic.ac.uk>

--- src/klee_web/urls.py
+++ src/klee_web/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 import api
