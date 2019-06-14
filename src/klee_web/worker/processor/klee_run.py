aaa589e96f5ea5aeca406cc45f948b1aba5b0953
Denis Gavrielov <de.gavrielov@gmail.com>
2019-06-14 17:32:07 +0100

    add todo

--- src/klee_web/worker/processor/klee_run.py
+++ src/klee_web/worker/processor/klee_run.py
@@ -39,6 +39,7 @@ class KleeRunProcessor(BaseProcessor):
         return result
 
     def create_klee_command(self, arg_list):
+        # TODO: possibly reduce this to 'klee' in the future.
         klee_command = ['/home/klee/klee_build/bin/klee']
         if arg_list:
             klee_command += ['--posix-runtime', '-libc=uclibc']
