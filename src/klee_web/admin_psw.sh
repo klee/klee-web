f48959d8dad697b723427afa186f2e50046fb3bb
Denis Gavrielov <de.gavrielov@gmail.com>
2019-07-02 14:28:27 +0100

    working state of dockerized django and postgres

--- src/klee_web/admin_psw.sh
+++ src/klee_web/admin_psw.sh
@@ -2,4 +2,4 @@
 
 . /etc/profile.d/klee-web-environment.sh
 cd /titb/src/klee_web
-python manage.py update_admin_user --username=admin --password={{ admin_password }}
+python manage.py update_admin_user --username=admin --password=development
