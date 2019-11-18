4299d441fe57929e9b5358e4d6345310aabca652
Anastasios Andronidis <anastasis90@yahoo.gr>
2019-11-18 15:31:26 +0000

    Enable flake8 for kleeweb

--- src/klee_web/build.sh
+++ src/klee_web/build.sh
@@ -4,7 +4,11 @@ set -e
 
 CUR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+echo "Running flake8"
+flake8 --ignore=E722 --max-complexity 12 --exclude=migrations "${CUR_DIR}"
+
 echo "Downloading GeoIP"
+
 mkdir -p "${CUR_DIR}/geoip"
 wget -q -P "${CUR_DIR}/geoip" https://geolite.maxmind.com/download/geoip/database/GeoLite2-City.mmdb.gz
 wget -q -P "${CUR_DIR}/geoip" https://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.mmdb.gz
