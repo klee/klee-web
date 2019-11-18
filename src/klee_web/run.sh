b3a3aa1aea006a7710f334eeb78d3b7cb811ba65
Anastasios Andronidis <anastasis90@yahoo.gr>
2019-11-18 15:31:26 +0000

    Enable flake8 for kleeweb

--- src/klee_web/run.sh
+++ src/klee_web/run.sh
@@ -3,5 +3,5 @@
 set -e
 
 python manage.py migrate --noinput
-python manage.py update_admin_user --username=admin --password="$ADMIN_PASSWORD"
+python manage.py update_admin_user --username=admin --password="${ADMIN_PASSWORD}"
 uwsgi --master --uid www-data --gid www-data -s /tmp/uwsgi.sock -w wsgi:application --chmod-socket=666
